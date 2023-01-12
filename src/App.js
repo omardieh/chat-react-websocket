c121b2730df1a4f8ba19e14f88934887a61856c7
omardiehs <omar.dieh@wildcodeschool.com>
2023-01-13 00:27:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { NavLink } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          {["Home", "Live Chat", "Login"].map((e, i) => (
+            <NavLink
+              key={i}
+              className={({ isActive }) => (isActive ? "active" : "inactive")}
+            >
+              {e}
+            </NavLink>
+          ))}
+        </nav>
       </header>
+      <aside></aside>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
